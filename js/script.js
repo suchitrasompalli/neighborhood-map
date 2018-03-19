484100f847479ff25da8ed06235231b34fede62e
Suchitra Sompalli <suchitra.sompalli@gmail.com>
2018-03-19 12:59:34 -0500

    bug: https for marker image

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ function toggleBounce(marker, number_of_bounces) {
  of 0, 0 and be anchored at 10, 34). */
 function makeMarkerIcon(markerColor) {
     let markerImage = new google.maps.MarkerImage(
-        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+        'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
         '|40|_|%E2%80%A2',
         new google.maps.Size(21, 34),
         new google.maps.Point(0, 0),
